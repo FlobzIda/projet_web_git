87957b55538ef1133b6ce129836bfe9d83b542f6
MathAvecH <math.vassal@gmail.com>
2025-01-29 11:12:35 +0100

    ajout des uuid dans le stockage des csv

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
 from xgboost import XGBClassifier
 import os
+import uuid
+import json
 
-from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
@@ -17,25 +19,58 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
-# Store uploaded CSV data
 data = {}
+
+USERS_FILES_JSON = 'users_files.json'
+
+def save_file_mapping(original_filename, new_filename):
+    file_mappings = {}
+    
+    if os.path.exists(USERS_FILES_JSON) and os.path.getsize(USERS_FILES_JSON) > 0:
+        try:
+            with open(USERS_FILES_JSON, 'r') as f:
+                file_mappings = json.load(f)
+        except json.JSONDecodeError:
+            file_mappings = {}
+    
+    file_mappings[original_filename] = new_filename
+    
+    with open(USERS_FILES_JSON, 'w') as f:
+        json.dump(file_mappings, f, indent=4)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
         app.logger.error('No file part in the request')
         return jsonify({'error': 'No file part in the request'}), 400
+    
     file = request.files['file']
+    
     if file.filename == '':
         app.logger.error('No selected file')
         return jsonify({'error': 'No selected file'}), 400
+    
     if file:
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        # Générer un UUID v4 pour nommer le fichier
+        file_extension = os.path.splitext(file.filename)[1]
+        unique_filename = f"{uuid.uuid4()}{file_extension}"
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], unique_filename)
+        
         file.save(file_path)
         app.logger.info(f'File saved at {file_path}')
-        global data
-        data['df'] = pd.read_csv(file_path)
-        return jsonify({'columns': data['df'].columns.tolist()})
+        
+        # Enregistrer la correspondance dans users_files.json
+        save_file_mapping(file.filename, unique_filename)
+        
+        try:
+            print(file_path)
+            data = pd.read_csv(file_path)
+            print(data.columns.tolist())
+            return jsonify({'columns': data.columns.tolist()})
+        except Exception as e:
+            app.logger.error(f'Error processing file: {str(e)}')
+            return jsonify({'error': 'Error processing file'}), 500
+    
     app.logger.error('File upload failed')
     return jsonify({'error': 'File upload failed'}), 500
 
