2c62bb864f8537949317cd6ebcd6fb3b8804889d
MathAvecH <math.vassal@gmail.com>
2025-01-30 21:03:04 +0100

    Colonnes X et Y

--- frontend/src/components/Form2.vue
+++ frontend/src/components/Form2.vue
@@ -1,10 +1,12 @@
 <template>
     <v-form v-model="isValid" ref="form">
         <v-container>
-            
             <v-row>
                 <v-col cols="12" md="6">
-                    <v-select multiple clearable :items="columnsForm2"
+                    <v-select
+                        multiple
+                        clearable
+                        :items="availableColumnsX"
                         label="Sélectionner les colonnes X"
                         v-model="selectedColumnsX"
                         @update:modelValue="handleInput1Change"
@@ -12,7 +14,9 @@
                     </v-select>
                 </v-col>
                 <v-col cols="12" md="6">
-                    <v-select clearable :items="columnsForm2"
+                    <v-select
+                        clearable
+                        :items="availableColumnsY"
                         label="Sélectionner la colonne Y"
                         v-model="selectedColumnsY"
                         @update:modelValue="handleInput2Change"
@@ -22,7 +26,12 @@
             </v-row>
             <v-row>
                 <v-col cols="12">
-                    <v-data-table :headers="previewHeaders" :items="preview" item-value="name" class="elevation-1"></v-data-table>
+                    <v-data-table
+                        :headers="previewHeaders"
+                        :items="preview"
+                        item-value="name"
+                        class="elevation-1">
+                    </v-data-table>
                 </v-col>
             </v-row>
             <v-btn @click="submitSelection" items="fileName">Confirm Selection</v-btn>
@@ -43,38 +52,31 @@ export default {
             }
         };
     },
-    watch: {
-        selectedColumnsX(newVal) {
-            console.log("selectedColumnsX changed:", newVal);
-            this.validateForm();
-        },
-        selectedColumnsY(newVal) {
-            console.log("selectedColumnsY changed:", newVal);
-            this.validateForm();
-        }
-    },
     computed: {
+        availableColumnsX() {
+            return this.columnsForm2.filter(col => !this.selectedColumnsY.includes(col));
+        },
+        availableColumnsY() {
+            return this.columnsForm2.filter(col => !this.selectedColumnsX.includes(col));
+        },
         previewHeaders() {
             return this.preview.length > 0 ? Object.keys(this.preview[0]).map(key => ({ text: key, value: key })) : [];
         }
     },
     methods: {
         submitSelection(items) {
-            this.$emit("selected", this.selectedColumnsX, this.selectedColumnsY,this.fileName);
-            },
+            this.$emit("selected", this.selectedColumnsX, this.selectedColumnsY, this.fileName);
+        },
         async validateForm() {
             console.log("validateForm()")
-
             const valid = this.selectedColumnsX.length > 0 && this.selectedColumnsY.length > 0;
             this.$emit("form2ValidateEmit", valid);
         },
-
         handleInput1Change(event) {
             console.log("handleInput1Change()")
             this.selectedColumnsX = event;
             this.validateForm();
         },
-
         handleInput2Change(event) {
             console.log("handleInput2Change()")
             this.selectedColumnsY = event;
